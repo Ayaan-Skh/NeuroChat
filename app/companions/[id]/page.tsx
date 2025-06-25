c770c39334b7ee1ea55515b30fd2e2ddc4e49648
Ayaan-Skh <ayaanskh23@gmail.com>
2025-06-26 02:41:04 +0530

    implemented nav bar

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanionSessions = () => {
+  return (
+    <div>
+      Your companion sessions
+    </div>
+  )
+}
+
+export default CompanionSessions
---
+++ app/subscription/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Subscriptions = () => {
+  return (
+    <div>
+      Subscriptions
+      <p>Manage your subscriptions here.</p>
+    </div>
+  )
+}
+
+export default Subscriptions
